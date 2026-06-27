6518728aea644039d5b38d398b8b47edc4169ef0
maruti swiggy <marutiswiggy@gmail.com>
2026-06-28 00:21:07 +0530

    robo-dev-env

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "az_info" {
    value = data.aws_availability_zones.available.names
 }
+
+output "vpc_id" {
+  value = aws.vpc.main.id
+}
+
+output "public_sub_ids" {
+  value = aws_subnet.public_sub_1[*].ids
+}
+
+output "private_sub_ids" {
+  value = aws_subnet.private_sub_1[*].ids
+}
+
+output "database_sub_ids"{
+    value = aws_subnet.database_sub_1[*].ids
+}
